0c2ece5605bcce8766b5b6eeda7972e24e7fbc14
Sandi1802 <sandipranata1802@gmail.com>
2026-09-17 07:42:31 +0700

    fix: move paprika routes outside hydroponics prefix

--- routes/web.php
+++ routes/web.php
@@ -120,17 +120,7 @@ Route::middleware('auth')->group(function () {
             Route::get('/notifications', [\App\Http\Controllers\HydroponicController::class, 'getNotifications'])->name('hydroponics.notifications');
             Route::post('/notifications/read', [\App\Http\Controllers\HydroponicController::class, 'markNotificationsRead']);
 
-    // Paprika Routes
-    Route::prefix('paprika')->group(function () {
-        Route::get('/dashboard', [\App\Http\Controllers\PaprikaController::class, 'dashboard'])->name('paprika.dashboard');
-        Route::get('/greenhouses', [\App\Http\Controllers\PaprikaController::class, 'greenhouses'])->name('paprika.greenhouses');
-        Route::get('/greenhouses/{id}', [\App\Http\Controllers\PaprikaController::class, 'greenhouseDetail'])->name('paprika.greenhouses.detail');
-        Route::post('/greenhouses/bulk-update', [\App\Http\Controllers\PaprikaController::class, 'bulkUpdatePlants'])->name('paprika.greenhouses.bulk-update');
-        Route::get('/pemupukan', [\App\Http\Controllers\PaprikaController::class, 'pemupukan'])->name('paprika.pemupukan');
-        Route::post('/pemupukan', [\App\Http\Controllers\PaprikaController::class, 'storePemupukan'])->name('paprika.pemupukan.store');
-        Route::get('/penyemprotan', [\App\Http\Controllers\PaprikaController::class, 'penyemprotan'])->name('paprika.penyemprotan');
-        Route::post('/penyemprotan', [\App\Http\Controllers\PaprikaController::class, 'storePenyemprotan'])->name('paprika.penyemprotan.store');
-    });
+    
 
 
             // Daily Tasks
@@ -208,4 +198,16 @@ Route::middleware('auth')->group(function () {
         Route::delete('/penyemprotan/{id}', [\App\Http\Controllers\KonvensionalController::class, 'penyemprotanDestroy'])->name('konvensional.penyemprotan.destroy');
     });
 
+
+    // Paprika Routes
+    Route::prefix('paprika')->group(function () {
+        Route::get('/dashboard', [\App\Http\Controllers\PaprikaController::class, 'dashboard'])->name('paprika.dashboard');
+        Route::get('/greenhouses', [\App\Http\Controllers\PaprikaController::class, 'greenhouses'])->name('paprika.greenhouses');
+        Route::get('/greenhouses/{id}', [\App\Http\Controllers\PaprikaController::class, 'greenhouseDetail'])->name('paprika.greenhouses.detail');
+        Route::post('/greenhouses/bulk-update', [\App\Http\Controllers\PaprikaController::class, 'bulkUpdatePlants'])->name('paprika.greenhouses.bulk-update');
+        Route::get('/pemupukan', [\App\Http\Controllers\PaprikaController::class, 'pemupukan'])->name('paprika.pemupukan');
+        Route::post('/pemupukan', [\App\Http\Controllers\PaprikaController::class, 'storePemupukan'])->name('paprika.pemupukan.store');
+        Route::get('/penyemprotan', [\App\Http\Controllers\PaprikaController::class, 'penyemprotan'])->name('paprika.penyemprotan');
+        Route::post('/penyemprotan', [\App\Http\Controllers\PaprikaController::class, 'storePenyemprotan'])->name('paprika.penyemprotan.store');
+    });
 });
